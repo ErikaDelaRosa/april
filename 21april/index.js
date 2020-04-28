c21dadd19dbd946cc35f4ded88c2a56099c73b5c
ErikaDelaRosa <erika-delarosa@hotmail.com>
2020-04-28 10:33:54 +0200

    daily exercises

---
+++ 21april/index.js
@@ -0,0 +1,62 @@
+// where I can link my js file
+
+// Github assignment push
+
+// window.alert === alert
+
+// Zoom link always is
+// https://zoom.us/j/3551491935
+// Or you can join using ID
+// Meeting ID: 355 149 1935
+
+//
+// document.querySelectorAll() will return A non-live NodeList 🧐
+// -NodeList-
+// NodeList objects are collections of nodes, usually returned by properties such as Node.childNodes and methods such as document.querySelectorAll().
+
+// -Node-
+// In the context of the DOM, a node is a single point in the node tree. Various things that are nodes are the document itself, elements, text, and comments.
+
+// Node tree https://dom.spec.whatwg.org/#concept-node
+
+let avatar = document.querySelector(".img");
+console.log(avatar);
+//avatar.innerHTML = "cool";
+// let list = avatar.querySelectorAll("span");
+let list = document.querySelectorAll(".img span");
+console.log(list);
+
+list.forEach((item) => {
+  console.log(item);
+  item.innerText = "nice";
+  item.style.backgroundColor = "red";
+});
+console.log(list.length);
+list[2].style.color = "pink";
+
+let navList = document.querySelector('#menuList');
+console.log(navList);
+// more selectors we can use > + ~ 
+let specialOnes = navList.querySelectorAll("li[class='highlight']");
+console.log(specialOnes);
+specialOnes[0].innerText = "Nice one"; 
+let moreClass = document.querySelector('.main'); 
+
+moreClass.classList.add('small'); 
+moreClass.classList.add('textRtl', 'flexSize'); 
+let classArr = ['classOne','classTwo'];
+
+// adding more classes to the div using...
+moreClass.classList.add(...classArr);
+console.log(moreClass.classList);
+
+moreClass.style.color ="blue";
+moreClass.style.backgroundColor = "gray";
+moreClass.style.fontSize = "1.5rem";
+// font-size in js fontSize
+moreClass.style.marginLeft = "5rem";
+moreClass.style.display= 'flex';
+moreClass.style.justifyContent= 'center';
+
+document.write("Hi, i am hadi");
+document.write("<h1> cool thsi is me </h1>")
---
+++ 22april/index.js
@@ -0,0 +1,20 @@
+document.write("hey, this is me");
+document.write("<h3> erika </h3>");
+
+const create=()=>{
+    let newElement = document.createElement('div');
+    let newText = document.createTextNode('I am a new element');
+    //console.log(newElement);
+    newElement.appendChild(newText);
+    document.body.appendChild(newElement);
+};
+
+const addToList = () => {
+    let userInput = document.querySelector('#userText').value;
+    console.log(userInput);
+    let newLi = document.createElement("li");
+    let text = document.createTextNode(userInput);
+    newLi.appendChild(text);
+    document.querySelector('.result').appendChild(newLi);
+    document.querySelector("#userText").value ="";
+};
